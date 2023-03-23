551968f91e25f2783455a15fd219c087ea463cd2
DanielB <dbruijn164@gmail.com>
2023-03-23 09:52:02 +0100

    Registratiepagina gemaakt

--- site/database.php
+++ site/database.php
@@ -6,7 +6,7 @@ $password = "password";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=receptenboek", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    //echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
--- site/inlog.php
+++ site/inlog.php
@@ -41,17 +41,13 @@ if (isset($_POST['voornaam'])) {
 </head>
 
 <body>
-    <h1>Registreren</h1>
+    <h1>Inloggen</h1>
     <form id="FormRegister" method="post">
-        <label for="voornaam">Voornaam</label></br>
-        <input type="text" name="voornaam" id="voornaam"></br>
-        <label for="achternaam">Achternaam</label></br>
-        <input type="text" name="achternaam" id="achternaam"></br>
         <label for="email">Email</label></br>
         <input type="email" name="email" id="email"></br>
         <label for="wachtwoord">Wachtwoord</label></br>
         <input type="password" name="wachtwoord" id="wachtwoord"></br>
-        <input type="submit" value="Registreer">
+        <input type="submit" value="Inloggen">
     </form>
 
 </body>
